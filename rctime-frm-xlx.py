20c4fb1b1bc43904054af972515d6fa445e2f75b
gzhawk <gzhawk@hotmail.com>
2022-09-13 14:46:09 +0800

    Update rctime-frm-xlx.py
    seperate the 'average' and 'list' function

--- rctime-frm-xlx.py
+++ rctime-frm-xlx.py
@@ -1,30 +1,28 @@
 """
 It's collecting the B5K's re-convergent data, based on below rules:
+1. record the period which fix type from RTX(9) to none RTX as RTX Lost Period
 
-1. record the period which fix type from RTX(9) to none RTX
+2. when acitve "average" value for re-convergent calculation (Avg_o_List == 0)
+2.1 when it goes back to RTX from lost, skip c_skip number ahead data (from previous lost RTX position)
+2.2 then start collect continuous c_anum numbers of average "Pos Accy (m)" value (accy_1) as comparision base number
+2.3 check continuous c_anum numbers of average "Pos Accy (m)" value (accy_2) beginning with RTX recovered point
+2.4 once accy_2 <= accy_1, consider this preriod as re-convergent time
 
-2. after fix type changed and goes back to RTX, start re-convergent calculating
-2.1 when it goes back to RTX, skip 10 ahead data (from previous before lost RTX accuracy)
-2.2 then start collect 10 numbers of average accy_1 data as comparision base number
-2.3 check the 10 numbers of average accy_2 data beginning with RTX recovered point
-2.4 once meet (accy_2 < accy_1), consider this preriod as re-convergent time
-
-for example:
-
-time tick: 1,2,3,...,99
-RTX lost at tick 25, return to RTX at tick 30
-accy_1 will be average accuracy number from tick 5~14
-accy_2 will be average accuracy number from tick 31~40
+3. when acitve "list" value for re-convergent calculation (Avg_o_List == 1)
+3.1 when it goes back to RTX from lost, only compare the "Pos Accy (m)" value with the numbers in x_list
 """
-xver = '0.2'
+
+xver = '0.3'
 import os
 import openpyxl
 
-#xlx_path = 'C:\\Work\\Tools\\XLX2NMEA\\'
-xlx_path = '/Users/Hawk/Downloads/'
-xlx_name = '22020831_sv_example'
+xlx_path = 'C:\\Work\\Tools\\rctime_from_xlx\\'
+#xlx_path = '/Users/Hawk/Downloads/'
+xlx_name = 'sv_example_22020831'
 xlx_tail = '.xlsx'
-output_tail = '.txt'
+output_tail = '.csv'
+Avg_o_List  = 0 # 0: Avg, 1: List
+x_list      = [0.1, 0.05, 0.02] # TBD: it has to be only 3, and has to be left one larger than right one
 
 xlx_wb = openpyxl.load_workbook(xlx_path+xlx_name+xlx_tail)
 xlx_sht = xlx_wb[xlx_wb.sheetnames[0]]
@@ -36,6 +34,8 @@ x_lon       = 9
 x_lat       = 10
 x_posacc    = 29
 x_gps_max   = 86400
+x_mark      = [0, 0, 0]
+x_gps       = [0, 0, 0]
 c_count     = 0
 c_trig      = 9
 c_flag      = 0
@@ -46,12 +46,29 @@ c_acc_1     = 0
 c_acc_2     = 0
 c_row_fix   = 0
 
-print('\n\nRe-convergent time version:',xver)
-# Open a file for exclusive creation. If the file already exists, the operation fails.
-#with open(xlx_path+xlx_name+output_tail, 'x') as c_log:
+if Avg_o_List == 1:
+    output_file = xlx_path+xlx_name+'_list'+output_tail
+else:
+    output_file = xlx_path+xlx_name+'_avg'+output_tail
 
-# Opens a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
-with open(xlx_path+xlx_name+output_tail, 'w') as c_log:
+# x: Open a file for exclusive creation. If the file already exists, the operation fails.
+# w: Open a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
+with open(output_file, 'w') as c_log:
+    c_msg = '\nRe-convergent time tool version:' + xver
+    c_msg += '\n'
+    c_msg += '\n err1 - fix lost again'
+    c_msg += '\n err2 - avg: previous invalid previous row'
+    c_msg += '\n err3 - avg: previous row too short'
+    c_msg += '\n err4 - flag error'
+    c_msg += '\n'
+    
+    if Avg_o_List == 0:
+        c_msg += '\nIndex,StartLon,StartLat,StartTime,EndLon,EndLat,EndTime,RTXLostPeriod(s),StartAccy,EndAccy,ReCnvtPeriod(s)'
+    else:
+        c_msg += '\nIndex,StartLon,StartLat,StartTime,EndLon,EndLat,EndTime,RTXLostPeriod(s),'+str(x_list[0])+'M(s),'+str(x_list[1])+'M(s),'+str(x_list[2])+'M(s)'
+
+    print(c_msg)
+    c_log.write(str(c_msg))
     for row_1 in range(2, xlx_sht.max_row):
         if c_trig != xlx_sht.cell(row_1, x_fix).value:
             if c_flag == 0: # Never goes into fix yet
@@ -61,24 +78,29 @@ with open(xlx_path+xlx_name+output_tail, 'w') as c_log:
                     c_count += 1
                     c_gps = xlx_sht.cell(row_1, x_gpstime).value
                     c_msg = '\n'
-                    c_msg += str(c_count).zfill(3)
-                    c_msg += ' (LON,LAT)Time'
-                    c_msg += '\n    ('
-                    c_msg += str(xlx_sht.cell(row_1, x_lon).value)
+                    c_msg += str(c_count).zfill(3) # Index
                     c_msg += ','
-                    c_msg += str(xlx_sht.cell(row_1, x_lat).value)
-                    c_msg += ')'
-                    c_msg += str(xlx_sht.cell(row_1, x_time).value)
-                    # Keep it as for later calculation
+                    c_msg += str(xlx_sht.cell(row_1, x_lon).value) # StartLon
+                    c_msg += ','
+                    c_msg += str(xlx_sht.cell(row_1, x_lat).value) # StartLat
+                    c_msg += ','
+                    c_msg += str(xlx_sht.cell(row_1, x_time).value) # StartTime
+                    # Keep item index for later calculation
                     c_row_fix = row_1
                 else:
                     continue
-            else: # Fix lost again before finished the convergent calculate
+            # Fix lost again before finished the convergent calculate
+            else: 
+                if Avg_o_List == 1:
+                    x_mark[0] = 0 
+                    x_mark[1] = 0 
+                    x_mark[2] = 0
                 c_flag = 0
                 c_gps = 0
-                c_msg += '...) Fix lost, process abort'
+                c_msg += ',err1' # Fix lost, process abort
                 print(c_msg)
                 c_log.write(str(c_msg))
+                continue
         else:
             if c_flag == 0: # From no fix goes into fix
                 c_flag = 1
@@ -90,69 +112,104 @@ with open(xlx_path+xlx_name+output_tail, 'w') as c_log:
                         c_gps += xlx_sht.cell(row_1, x_gpstime).value
                     else:
                         c_gps = xlx_sht.cell(row_1, x_gpstime).value - c_gps
-                    c_msg += '~('
-                    c_msg += str(xlx_sht.cell(row_1, x_lon).value)
                     c_msg += ','
-                    c_msg += str(xlx_sht.cell(row_1, x_lat).value)
-                    c_msg += ')'
-                    c_msg += str(xlx_sht.cell(row_1, x_time).value)
-                    c_msg += '\n    RTX lost(s):'
-                    c_msg += str(format(c_gps,'.2f'))
-                    print(c_msg)
-                    c_log.write(str(c_msg))
+                    c_msg += str(xlx_sht.cell(row_1, x_lon).value) # EndLon
+                    c_msg += ','
+                    c_msg += str(xlx_sht.cell(row_1, x_lat).value) # EndLat
+                    c_msg += ','
+                    c_msg += str(xlx_sht.cell(row_1, x_time).value) # EndTime
+                    c_msg += ','
+                    c_msg += str(format(c_gps,'.2f')) # RTXLostPeriod(s)
                     # Start the re-convergent calculation
                     c_acc_1 = 0
                     c_flag = 2 
+                    # Keep it for later calculation
                     c_gps = xlx_sht.cell(row_1, x_gpstime).value
-                    # TBD: not handling c_row_fix < (c_skip + c_anum)
-                    if c_row_fix == 0 or c_row_fix < (c_skip + c_anum):
+                    if Avg_o_List == 1:
+                        x_gps[0] = xlx_sht.cell(row_1, x_gpstime).value
+                        x_gps[1] = xlx_sht.cell(row_1, x_gpstime).value
+                        x_gps[2] = xlx_sht.cell(row_1, x_gpstime).value
+                    
+                    if Avg_o_List == 1:
+                        continue
+
+                    # Average of c_anum numbers of data after skip c_skip numbers of data 
+                    if c_row_fix == 0:
                         c_flag = 0
                         c_gps = 0
-                        c_msg += '\n----Pre-fix row error, process abort'
+                        c_msg += ',err2' # fix row error, process abort
+                        print(c_msg)
+                        c_log.write(str(c_msg))
+                        continue
+                    elif c_row_fix < (c_skip + c_anum):
+                        c_flag = 0
+                        c_gps = 0
+                        c_msg += ',err3' # fix row skip error, process abort
                         print(c_msg)
                         c_log.write(str(c_msg))
                         continue
                     row_2 = c_row_fix - c_skip - c_anum 
-
-                    # Average of c_anum numbers of data after skip c_skip numbers of data 
                     for row_2 in range(row_2, row_2+c_anum):
                         c_acc_1 +=  xlx_sht.cell(row_2, x_posacc).value
                     c_acc_1 /= c_anum
-
-                    c_msg = '\n    Accy ('
-                    c_msg += str(format(c_acc_1,'.2f')) 
-                    c_msg += ' to '
+                    c_msg += ','
+                    c_msg += str(format(c_acc_1,'.2f')) # StartAccy
             elif c_flag == 2: # Fix back, start re-convergent calculation
-                # Start to get average of c_anum number of data
-                if c_acc_1 >= xlx_sht.cell(row_1, x_posacc).value:
-
-                    # Average of c_anum numbers of data
-                    c_acc_2 = 0
-                    row_2 = row_1
-                    for row_2 in range(row_2, row_2+c_anum):
-                        c_acc_2 +=  xlx_sht.cell(row_2, x_posacc).value
-                    c_acc_2 /= c_anum
-
-                    # Check if the average lower than expected
-                    if c_acc_1 < c_acc_2:
-                        continue
-                    # TBD: not handling c_gps == 0
-                    if xlx_sht.cell(row_1, x_gpstime).value < c_gps:
-                        c_gps = x_gps_max - c_gps
-                        c_gps += xlx_sht.cell(row_1, x_gpstime).value
+                if Avg_o_List == 1:
+                    for i in range(0, 2):
+                        if x_mark[i] == 0:
+                            if x_list[i] >= xlx_sht.cell(row_1, x_posacc).value:
+                                c_gps = x_gps[i]
+                                # TBD: not handling c_gps == 0
+                                if xlx_sht.cell(row_1, x_gpstime).value < c_gps:
+                                     c_gps = x_gps_max - c_gps
+                                     c_gps += xlx_sht.cell(row_1, x_gpstime).value
+                                else:
+                                     c_gps = xlx_sht.cell(row_1, x_gpstime).value - c_gps
+                                c_msg += ','
+                                c_msg += str(format(c_gps,'.2f')) 
+                                x_gps[i]  = c_gps
+                                x_mark[i] = 1
+                    if x_mark[0] !=0 and x_mark[1] !=0 and x_mark[2] != 0:
+                        x_mark[0] = 0 
+                        x_mark[1] = 0 
+                        x_mark[2] = 0
                     else:
-                        c_gps = xlx_sht.cell(row_1, x_gpstime).value - c_gps
-                    c_msg += str(format(c_acc_2,'.2f'))
-                    c_msg += ') ' 
-                    c_msg += str(format(c_gps,'.2f'))
-                    print(c_msg)
-                    c_log.write(str(c_msg))
-                    c_flag = 0
-                    c_gps = 0
+                        continue
+                else:
+                    # Start to get average of c_anum number of data
+                    if c_acc_1 >= xlx_sht.cell(row_1, x_posacc).value:
+                        # Average of c_anum numbers of data
+                        c_acc_2 = 0
+                        row_2 = row_1
+                        for row_2 in range(row_2, row_2+c_anum):
+                            c_acc_2 +=  xlx_sht.cell(row_2, x_posacc).value
+                        c_acc_2 /= c_anum
+                        # Check if the average lower than expected
+                        if c_acc_1 < c_acc_2:
+                            continue
+                        # TBD: not handling c_gps == 0
+                        if xlx_sht.cell(row_1, x_gpstime).value < c_gps:
+                            c_gps = x_gps_max - c_gps
+                            c_gps += xlx_sht.cell(row_1, x_gpstime).value
+                        else:
+                            c_gps = xlx_sht.cell(row_1, x_gpstime).value - c_gps
+
+                        c_msg += ','
+                        c_msg += str(format(c_acc_2,'.2f')) # EndAccy
+                        c_msg += ',' 
+                        c_msg += str(format(c_gps,'.2f')) # ReCnvtPeriod(s)
+                    else:
+                        continue
+                print(c_msg)
+                c_log.write(str(c_msg))
+                c_flag = 0
+                c_gps = 0
             else:
                 c_flag = 0
                 c_gps = 0
-                c_msg += '\n----Flag error, process abort'
+                c_msg += '\nerr4' # Flag error, process abort
                 print(c_msg)
                 c_log.write(str(c_msg))
-print('Log path:',xlx_path+xlx_name+output_tail)
+c_msg = '\nLog path:' + output_file
+print(c_msg)
